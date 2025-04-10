5a9724bd865ef3382b0abe7cbdb54e7bf32687f6
Dorytos13 <doriane.rosset@heig-vd.ch>
2025-04-10 13:17:27 +0200

    modifications du popup

--- Projet/ghibli/src/film/popup.js
+++ Projet/ghibli/src/film/popup.js
@@ -124,7 +124,7 @@ export function setupFilmPopup(filmsData, getCurrentIndex) {
                                 style="border-radius:12px" 
                                 src="https://open.spotify.com/embed/album/${film.spotifyPlaylistId}?utm_source=generator&theme=0" 
                                 width="100%" 
-                                height="352" 
+                                height="152" 
                                 frameBorder="0" 
                                 allowfullscreen="" 
                                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
